c5d021286646317f15111e33939efb5b0f827f2b
Vishal Jani <vishaljani04@gmail.com>
2026-03-28 22:14:13 +0530

    Deployement error solve

--- server/config/passport.js
+++ server/config/passport.js
@@ -2,11 +2,12 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const User = require('../models/User');
 
-passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/api/auth/google/callback"
-  },
+if (process.env.GOOGLE_CLIENT_ID && process.env.GOOGLE_CLIENT_SECRET) {
+  passport.use(new GoogleStrategy({
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "/api/auth/google/callback"
+    },
   async (accessToken, refreshToken, profile, done) => {
     try {
       let user = await User.findOne({ email: profile.emails[0].value });
@@ -25,8 +26,10 @@ passport.use(new GoogleStrategy({
     } catch (err) {
       return done(err);
     }
-  }
-));
+  }));
+} else {
+  console.warn("WARNING: Google OAuth credentials missing. Google Login will be disabled.");
+}
 
 passport.serializeUser((user, done) => {
   done(null, user.id);
